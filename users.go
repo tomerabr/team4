c13641b4b0b63862c13fb2badb2885e3b9feafe1
Tomer Abraham <tomerabr22@gmail.com>
2021-02-15 18:45:42 +0000

    structs

--- users.go
+++ users.go
@@ -1,15 +1,9 @@
 package main
 
-import (
-	"database/sql"
-	"encoding/json"
-	"fmt"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
+import "time"
 
 type User struct {
+	ID			int	   `json:"id"`
 	Username 	string `json:"username"`
 	Password 	string `json:"password"`
 	Base_ID  	string `json:"base_ID"`
@@ -17,4 +11,33 @@ type User struct {
 	Capsule_ID 	string `json:"capsule_ID"`
 	Permissions []bool `json:"permissions"`
 	Admin 		[]bool `json:"admin"`
+	TimeCreated string `json:"time"`
+}
+
+type Space struct {
+	Name	string `json:"name"`
+	Base	string `json:"base_name"`
+}
+
+type Capsule struct {
+	Users	[]int `json:"users"`
+}
+
+type Branch struct {
+	Name	string `json:"name"`
+	Users	[]int `json:"users"`
+}
+
+type Base struct {
+	Name	string `json:"name"`
+	Spaces	[]int `json:"spaces"`
+}
+
+type Schedule struct {
+	Space	int `json:"id"`
+	Start	string `json:"start"`
+	End		string `json:"end"`
+	Date	time.Time `json:"date"`
+	Capsule_ID int	`json:"capsule"`
+	User_ID		int `json:"user"`
 }
